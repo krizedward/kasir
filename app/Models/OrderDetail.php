1fbbe9bade536a3a0c1b1c46bc0a99c69bbdb2a9
krizedward <edward.mangare@binus.ac.id>
2020-11-06 03:32:00 +0800

    fix bug

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    //
+    protected $fillable=['id','order_id','product_id','product_name','product_price','qty','subtotal']
 }
